089eafc62174b8f297fae72645bb1b499cc2ba1d
sadjokerwin <al107ivanov@gmail.com>
2023-11-09 17:42:22 +0200

    member

--- Week 4/Gym/src/bg/sofia/uni/fmi/mjt/gym/member/Member.java
+++ Week 4/Gym/src/bg/sofia/uni/fmi/mjt/gym/member/Member.java
@@ -1,4 +1,74 @@
 package bg.sofia.uni.fmi.mjt.gym.member;
 
-public class Member {
+import bg.sofia.uni.fmi.mjt.gym.workout.Workout;
+
+import java.time.DayOfWeek;
+import java.util.Collection;
+import java.util.Map;
+
+public class Member implements GymMember{
+    private String name;
+    private int age;
+    private String personalIdNumber;
+    private Gender gender;
+    private Address address;
+    private Map<DayOfWeek, Workout> trainingProgram;
+
+    public Member(Address address, String name, int age, String personalIdNumber, Gender gender) {
+        this.name = name;
+        this.age = age;
+        this.personalIdNumber = personalIdNumber;
+        this.gender = gender;
+        this.address = address;
+    }
+
+    @Override
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public int getAge() {
+        return age;
+    }
+
+    @Override
+    public String getPersonalIdNumber() {
+        return personalIdNumber;
+    }
+
+    @Override
+    public Gender getGender() {
+        return gender;
+    }
+
+    @Override
+    public Address getAddress() {
+        return address;
+    }
+
+    @Override
+    public Map<DayOfWeek, Workout> getTrainingProgram() {
+        return trainingProgram;
+    }
+
+    @Override
+    public void setWorkout(DayOfWeek day, Workout workout) {
+        if (day == null || workout == null) {
+           throw new IllegalArgumentException("Day or workout is null");
+        }
+
+        trainingProgram.put(day, workout);
+    }
+
+    @Override
+    public Collection<DayOfWeek> getDaysFinishingWith(String exerciseName) {
+        if (exerciseName == null || exerciseName.isEmpty()) {
+           throw new IllegalArgumentException("Exercise Name is null or empty");
+        }
+
+
+    }
+
+
 }
