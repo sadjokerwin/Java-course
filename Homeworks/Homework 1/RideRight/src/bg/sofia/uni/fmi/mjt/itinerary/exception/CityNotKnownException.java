4ccecf644e5f2a164ffb53e52f9d6d29ff5a5cba
sadjokerwin <al107ivanov@gmail.com>
2023-11-27 17:04:20 +0200

    Init Homework

---
+++ Homeworks/Homework 1/RideRight/src/bg/sofia/uni/fmi/mjt/itinerary/exception/CityNotKnownException.java
@@ -0,0 +1,7 @@
+package bg.sofia.uni.fmi.mjt.itinerary.exception;
+
+public class CityNotKnownException extends Exception {
+    public CityNotKnownException(String message) {
+        super(message);
+    }
+}
---
+++ Homeworks/Homework 1/RideRight/src/bg/sofia/uni/fmi/mjt/itinerary/exception/NoPathToDestinationException.java
@@ -0,0 +1,7 @@
+package bg.sofia.uni.fmi.mjt.itinerary.exception;
+
+public class NoPathToDestinationException extends Exception {
+    public NoPathToDestinationException(String message) {
+        super(message);
+    }
+}
