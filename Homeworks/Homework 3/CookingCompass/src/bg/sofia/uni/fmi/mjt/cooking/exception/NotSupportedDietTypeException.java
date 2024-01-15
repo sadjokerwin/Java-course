d533ff8262a65e5bf348714e4a86d1e55f2ec40f
sadjokerwin <al107ivanov@gmail.com>
2024-01-16 00:08:59 +0200

    remove

--- Homeworks/Homework 3/CookingCompass/src/bg/sofia/uni/fmi/mjt/cooking/exception/NotSupportedDietTypeException.java
+++
@@ -1,7 +0,0 @@
-package bg.sofia.uni.fmi.mjt.cooking.exception;
-
-public class NotSupportedDietTypeException extends RuntimeException {
-    public NotSupportedDietTypeException(String message) {
-        super(message);
-    }
-}
--- Homeworks/Homework 3/CookingCompass/src/bg/sofia/uni/fmi/mjt/cooking/recipe/Recipe.java
+++
@@ -1,8 +0,0 @@
-package bg.sofia.uni.fmi.mjt.cooking.recipe;
-
-import java.util.List;
-
-public record Recipe(String label, List<String> dietLabels, List<String> healthLabels,
-                     List<String> ingredientLines, double totalWeight, List<String> cuisineType, List<String> mealType,
-                     List<String> dishType) {
-}
